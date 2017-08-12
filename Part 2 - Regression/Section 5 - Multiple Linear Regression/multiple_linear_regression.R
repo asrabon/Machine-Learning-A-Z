7c002074ad14669b28bf3c66e4c1ec6cddefdc5c
Sloan-PC\Sloan <asrabon@g.coastal.edu>
2017-08-12 13:01:54 -0400

    Multiple Linear Regression Completed

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -0,0 +1,34 @@
+# Multiple linear regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+#encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting multiple linear regression to the training set
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+# Predicting the test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
